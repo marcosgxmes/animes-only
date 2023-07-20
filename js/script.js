553800ec44d818508ccde979dc5a64eaf84eee1c
marcosgxmes <marcosgms777@gmail.com>
2023-07-20 14:10:21 -0300

    manutenção

--- js/script.js
+++ js/script.js
@@ -3,4 +3,19 @@ const navMenu = document.querySelector('.nav-menu');
 
 burgeBtn.addEventListener('click', () => {
     navMenu.classList.toggle('active');
-})
+});
+
+const imgs = document.getElementById('img');
+const img = document.querySelectorAll('#img img');
+
+let idy = 0;
+
+function carrosel() {
+    idy++;
+    if(idy > img.length -1) {
+        idy = 0;
+    }
+    imgs.style.transform = `translateX(${-idy * 280}px)`;
+}
+
+setInterval(carrosel, 3500);
