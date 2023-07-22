9f2969d7b10cfd21001d1f04982371ddd9970323
marcosgxmes <marcosgms777@gmail.com>
2023-07-22 17:25:12 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// MENU HAMBURGE
 const burgeBtn = document.querySelector('.hamburguer');
 const navMenu = document.querySelector('.nav-menu');
 
@@ -5,21 +6,57 @@ burgeBtn.addEventListener('click', () => {
     navMenu.classList.toggle('active');
 });
 
-const imgs = document.getElementById('img');
-const img = document.querySelectorAll('#img img');
+
+// CARROSEL
+const container = document.getElementById('img');
+const imgs = document.querySelectorAll('#img img');
 const bg = document.getElementById('bcg');
 
 let idy = 0;
 
-function carrosel() {
+if(`${screen.width < 600}`){
+    function smallCarrosel() {
+        idy++;
+    
+        if(idy > imgs.length -1) {
+            idy = 0;
+        }
+    
+    
+        container.style.transform = `translateX(${-idy * 280}px)`;
+        
+    
+        
+        if(bg.src == 'https://t4.ftcdn.net/jpg/04/06/48/21/360_F_406482145_y8lYwBqSNdi40Z7HotnzRPks1lJzNsHB.jpg') {
+            bg.src = 'https://img.freepik.com/free-vector/gradient-smooth-blue-lines-background_23-2148964962.jpg'
+        } else if ( bg.src == 'https://img.freepik.com/free-vector/gradient-smooth-blue-lines-background_23-2148964962.jpg') {
+            bg.src = 'https://t4.ftcdn.net/jpg/04/06/48/21/360_F_406482145_y8lYwBqSNdi40Z7HotnzRPks1lJzNsHB.jpg'
+        }
+      
+    };
+   
+    setInterval(smallCarrosel, 4300); 
+}
+
+
+
+
+if(screen.width > 600){
+clearInterval(smallCarrosel)
+}
+
+
+
+function bigCarrosel() {
     idy++;
 
-    if(idy > img.length -1) {
+    if(idy > imgs.length -1) {
         idy = 0;
     }
-    imgs.style.transform = `translateX(${-idy * 280}px)`;
 
 
+    container.style.transform = `translateX(${-idy * 400}px)`;
+    
     
     if(bg.src == 'https://t4.ftcdn.net/jpg/04/06/48/21/360_F_406482145_y8lYwBqSNdi40Z7HotnzRPks1lJzNsHB.jpg') {
         bg.src = 'https://img.freepik.com/free-vector/gradient-smooth-blue-lines-background_23-2148964962.jpg'
@@ -29,8 +66,10 @@ function carrosel() {
   
 };
 
-setInterval(carrosel, 4100);  
 
+
+
+//BOTAO VER MAIS
 const seeMoreBtn = document.querySelector('.see-more');
 const hiddenOntem = document.getElementById('hidden-ontem');
 
