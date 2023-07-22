1a307c9cf144f1fac853f98e0ea2e39a78f7dcb0
marcosgxmes <marcosgms777@gmail.com>
2023-07-22 18:10:54 -0300

    .

--- js/script.js
+++ js/script.js
@@ -7,15 +7,14 @@ burgeBtn.addEventListener('click', () => {
 });
 
 
-// CARROSEL
+// CARROSEL ATÉ 600px
 const container = document.getElementById('img');
 const imgs = document.querySelectorAll('#img img');
 const bg = document.getElementById('bcg');
 
 let idy = 0;
 
-if(`${screen.width < 600}`){
-    function smallCarrosel() {
+ function smallCarrosel() {
         idy++;
     
         if(idy > imgs.length -1) {
@@ -33,19 +32,14 @@ if(`${screen.width < 600}`){
             bg.src = 'https://t4.ftcdn.net/jpg/04/06/48/21/360_F_406482145_y8lYwBqSNdi40Z7HotnzRPks1lJzNsHB.jpg'
         }
       
-    };
+};
    
-    setInterval(smallCarrosel, 4300); 
-}
+setInterval(smallCarrosel, 4300); 
 
 
-
-
-if(screen.width > 600){
-clearInterval(smallCarrosel)
-}
-
-
+//CARROSEL ATÉ 768px
+const container2 = document.getElementById('img2');
+const imgs2 = document.querySelectorAll('#img2 img');
 
 function bigCarrosel() {
     idy++;
@@ -53,19 +47,11 @@ function bigCarrosel() {
     if(idy > imgs.length -1) {
         idy = 0;
     }
-
-
-    container.style.transform = `translateX(${-idy * 400}px)`;
     
-    
-    if(bg.src == 'https://t4.ftcdn.net/jpg/04/06/48/21/360_F_406482145_y8lYwBqSNdi40Z7HotnzRPks1lJzNsHB.jpg') {
-        bg.src = 'https://img.freepik.com/free-vector/gradient-smooth-blue-lines-background_23-2148964962.jpg'
-    } else if ( bg.src == 'https://img.freepik.com/free-vector/gradient-smooth-blue-lines-background_23-2148964962.jpg') {
-        bg.src = 'https://t4.ftcdn.net/jpg/04/06/48/21/360_F_406482145_y8lYwBqSNdi40Z7HotnzRPks1lJzNsHB.jpg'
-    }
-  
+    container2.style.transform = `translateX(${-idy * 400}px)`;  
 };
 
+setInterval(bigCarrosel, 4300); 
 
 
 
